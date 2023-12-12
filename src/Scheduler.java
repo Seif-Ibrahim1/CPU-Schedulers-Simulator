cd4055a7529322f340e1b40dc6359a860fb66266
ahmedsaad123456 <102562587+ahmedsaad123456@users.noreply.github.com>
2023-12-12 17:25:39 +0200

    update AG Schedule and add some comments

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -1,16 +1,40 @@
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+ * Scheduler abstract class that contains the common attributes and methods between all schedules.
+ */
 public abstract class Scheduler {
     protected ArrayList<Process> processes;
 
+    //==================================================================================================================
+
+    /**
+     * Constructor for Scheduler abstract class.
+     * sort the processes according to the Arrival Time.
+     *
+     * @param processes entered from the user.
+     */
     public Scheduler(ArrayList<Process> processes) {
         this.processes = processes;
         Collections.sort(this.processes);
     }
 
+    //==================================================================================================================
+
+    /**
+     * abstract method to run the algorithm for each subclass.
+     */
+
     public abstract void run();
 
+    //==================================================================================================================
+
+    /**
+     * calculate the Average of Turnaround Time.
+     *
+     * @return the Average of Turnaround Time.
+     */
     protected double getAverageTurnAroundTime() {
         double totalTurnAroundTime = 0;
         for (Process process : processes) {
@@ -18,6 +42,30 @@ public abstract class Scheduler {
         }
         return totalTurnAroundTime / processes.size();
     }
+
+    //==================================================================================================================
+
+    /**
+     * calculate the Average of waiting Time.
+     *
+     * @return the Average of waiting Time.
+     */
+    protected double getAverageWaitingTime() {
+        double totalWaitingTime = 0;
+        for (Process process : processes) {
+            totalWaitingTime += process.getWaitingTime();
+        }
+        return totalWaitingTime / processes.size();
+    }
+
+    //==================================================================================================================
+
+    /**
+     * set the waiting time for the process in the ArrayList.
+     *
+     * @param processId      of the process that it want to set the waiting time.
+     * @param newWaitingTime that will be given to the process.
+     */
     protected void modifyWaitingTime(int processId, int newWaitingTime) {
         for (Process process : processes) {
             if (process.getId() == processId) {
@@ -26,6 +74,33 @@ public abstract class Scheduler {
             }
         }
     }
+
+    //==================================================================================================================
+
+    /**
+     * set the turnaround time for the process in the ArrayList.
+     *
+     * @param processId          of the process that it want to set the turnaround time.
+     * @param newTurnaroundTime  that will be given to the process.
+     */
+    protected void modifyTurnaroundTime(int processId, int newTurnaroundTime) {
+        for (Process process : processes) {
+            if (process.getId() == processId) {
+                process.setTurnaroundTime(newTurnaroundTime);
+                return;
+            }
+        }
+    }
+
+    //==================================================================================================================
+
+    /**
+     *
+     * search for the process in the ArrayList by ID.
+     *
+     * @param id  of the process that we want to get it.
+     * @return the name of the process if exist and null if it doesn't exist.
+     */
     protected String getProcessNameById(int id) {
         for (Process process : processes) {
             if (process.getId() == id) {
@@ -36,33 +111,30 @@ public abstract class Scheduler {
         return null;
     }
 
-    protected void modifyTurnaroundTime(int processId, int newTurnaroundTime) {
-        for (Process process : processes) {
-            if (process.getId() == processId) {
-                process.setTurnaroundTime(newTurnaroundTime);
-                return;
-            }
-        }
-    }
+    //==================================================================================================================
 
-    protected double getAverageWaitingTime() {
-        double totalWaitingTime = 0;
-        for (Process process : processes) {
-            totalWaitingTime += process.getWaitingTime();
-        }
-        return totalWaitingTime / processes.size();
-    }
 
+    /**
+     * print the waiting time for each process.
+     */
     protected void printWaitingTime() {
         for (Process process : processes) {
             System.out.println("Process " + process.getName() + " waiting time: " + process.getWaitingTime());
         }
     }
 
+    //==================================================================================================================
+
+    /**
+     * print the Turnaround time for each process.
+     */
     protected void printTurnAroundTime() {
         for (Process process : processes) {
             System.out.println("Process " + process.getName() + " turnaround time: " + process.getTurnaroundTime());
         }
     }
+    //==================================================================================================================
 
 }
+
+//==================================================================================================================
