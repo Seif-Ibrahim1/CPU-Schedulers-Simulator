8882b23a21921bf789a7821658f76c26977c7abe
maryamosama33 <maryamosama3383@gmail.com>
2023-12-12 02:29:05 +0200

    Add SJF GUI (testing)

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,33 @@
+import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scheduler scheduler;
+        ArrayList<Process> processes = new ArrayList<>();
+        processes.add(new Process("P1", 0, 4, 1, 1,Color.gray));
 
-        Scanner scanner = new Scanner(System.in);
+        processes.add(new Process("P2", 2, 3, 2, 2,Color.PINK));
 
-        // Get the number of processes
-
-        // Create an ArrayList to store the processes
-        ArrayList<Process> processList = new ArrayList<>();
+        processes.add(new Process("P3", 4, 1, 3, 3,Color.yellow));
 
 
 
-            // Create a new Process object and add it to the ArrayList
-        Process process = new Process("p1", 0, 17, 4, 1);
-        processList.add(process);
+        Collections.sort(processes, Comparator.comparingInt(Process::getArrivalTime));
 
-        Process process1 = new Process("p2", 3, 6, 9, 2);
-        processList.add(process1);
+        SJFScheduling scheduler = new SJFScheduling(processes, 2);
 
-        Process process2 = new Process("p3", 4, 10, 2, 3);
-        processList.add(process2);
-
-        Process process3 = new Process("p4", 29, 4, 8, 4);
-
-        processList.add(process3);
-
-
-        scheduler = new AGScheduling(processList , 4);
-//        for (int i = 0 ; i<processList.size() ; i++) {
-//            System.out.println("Process " + processList.get(i).getId() + ": " +
-//                    "Name=" + processList.get(i).getName() +
-//                    ", Arrival Time=" + processList.get(i).getArrivalTime() +
-//                    ", Burst Time=" + processList.get(i).getBurstTime() +
-//                    ", Priority=" + processList.get(i).getPriority());
-//        }
         scheduler.run();
-
+        double averageWaitingTime = scheduler.getAverageWaitingTime();
+        double averageTurnaroundTime = scheduler.getAverageTurnAroundTime();
+        SwingUtilities.invokeLater(() -> {
+            GanttChartGUI chart = new GanttChartGUI(processes, averageWaitingTime, averageTurnaroundTime);
+            chart.setVisible(true);
+        });
     }
 }
 
--- src/Process.java
+++ src/Process.java
@@ -1,6 +1,7 @@
 public class Process {
     private String name;
     private int id;
+    private Color color;
     private int arrivalTime;
     private int burstTime;
     private int priority;
@@ -24,6 +25,7 @@ public class Process {
      * @param id             of the process.
      */
     public Process(String name, int arrivalTime, int burstTime, int priority, int id) {
+    public Process(String name, int arrivalTime, int burstTime, int priority, int id, Color color) {
         this.name = name;
         this.arrivalTime = arrivalTime;
         this.burstTime = burstTime;
@@ -35,6 +37,7 @@ public class Process {
         this.finishedTime = 0;
         this.id = id;
         this.startTime = 0;
+        this.color = color;
     }
 
     //==================================================================================================================
@@ -49,97 +52,66 @@ public class Process {
     public String getName() {
         return name;
     }
-
-    //==================================================================================================================
-
-
+    
     public int getArrivalTime() {
         return arrivalTime;
     }
-
-    //==================================================================================================================
-
-
 
     public int getBurstTime() {
         return burstTime;
     }
 
-    //==================================================================================================================
-
-
-
     public int getPriority() {
         return priority;
     }
-
-    //==================================================================================================================
 
     public int getTurnaroundTime() {
         return turnaroundTime;
     }
 
-    //==================================================================================================================
-
     public int getWaitingTime() {
         return waitingTime;
     }
-
-    //==================================================================================================================
 
     public int getRemainingBurstTime() {
         return remainingBurstTime;
     }
 
-    //==================================================================================================================
-
     public int getFinishedTime() {
         return finishedTime;
     }
 
-    //==================================================================================================================
-
     public int getId() {
         return id;
     }
-
-    //==================================================================================================================
+    public Color getColor() {return color;}
+    public int getStartTime() {return startTime;}
+    public void setStartTime(int startTime) {this.startTime = startTime;}
+    public void setColor(Color color) {this.color = color;}
 
     public void setTurnaroundTime(int turnaroundTime) {
         this.turnaroundTime = turnaroundTime;
     }
 
-    //==================================================================================================================
-
     public void setWaitingTime(int waitingTime) {
         this.waitingTime = waitingTime;
     }
-
-    //==================================================================================================================
 
     public void setRemainingBurstTime(int remainingBurstTime) {
         this.remainingBurstTime = remainingBurstTime;
     }
 
-    //==================================================================================================================
-
     public void setFinishedTime(int finishedTime) {
         this.finishedTime = finishedTime;
     }
-
-    //==================================================================================================================
 
     public void setName(String name) {
         this.name = name;
     }
 
-    //==================================================================================================================
-
     public void setPriority(int priority) {
         this.priority = priority;
     }
-
-    //==================================================================================================================
 
     public void setId(int id) {
         this.id = id;
