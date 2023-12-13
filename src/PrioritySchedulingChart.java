5883df2ee63909fa99d659e416b2d2b63822a4b1
maryamosama33 <maryamosama3383@gmail.com>
2023-12-13 21:16:11 +0200

    Add abstract class for GUI

---
+++ src/PrioritySchedulingChart.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+//public class PrioritySchedulingChart extends GUI{
+//
+//
+//}
--- src/SJFSchedulingChart.java
+++ src/SJFSchedulingChart.java
@@ -5,11 +5,7 @@ import java.util.ArrayList;
 /**
  * SJFSchedulingChart class extends JFrame to display a Gantt chart representing the Shortest Job First (SJF) scheduling algorithm.
  */
-public class SJFSchedulingChart extends JFrame {
-
-    private ArrayList<Process> processes;
-    private double averageWaitingTime;
-    private double averageTurnaroundTime;
+public class SJFSchedulingChart extends GUI{
     private int contextSwitchingTime;
 
     /**
@@ -21,28 +17,8 @@ public class SJFSchedulingChart extends JFrame {
      * @param contextSwitchingTime   Time taken for context switching
      */
     public SJFSchedulingChart(ArrayList<Process> processes, double averageWaitingTime, double averageTurnaroundTime, int contextSwitchingTime) {
-        this.processes = processes;
-        this.averageWaitingTime = averageWaitingTime;
-        this.averageTurnaroundTime = averageTurnaroundTime;
+        super(processes, averageWaitingTime, averageTurnaroundTime);
         this.contextSwitchingTime = contextSwitchingTime;
-        setTitle("Gantt Chart");
-        setSize(1200, 600);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-
-        JPanel panel = new JPanel() {
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                drawChart(g);
-            }
-        };
-
-        panel.setBackground(new Color(0x9AC0C0)); // Set background color
-        JScrollPane scrollPane = new JScrollPane(panel);
-        add(scrollPane);
-
-        setVisible(true);
     }
 
     /**
@@ -50,7 +26,7 @@ public class SJFSchedulingChart extends JFrame {
      *
      * @param g Graphics object used for drawing
      */
-    private void drawChart(Graphics g) {
+    protected void drawChart(Graphics g) {
         // Drawing the title
         Font titleFont = new Font("Arial", Font.BOLD, 23);
         g.setColor(Color.red);
@@ -84,71 +60,12 @@ public class SJFSchedulingChart extends JFrame {
             y += 50;
         }
         displayProcessInfo(g);
-        displayStatistics(g);
+        displayStatistics(g, "SJFSchedule");
     }
-
-    /**
-     * Displays process information on the chart.
-     *
-     * @param g Graphics object used for drawing
-     */
-    private void displayProcessInfo(Graphics g) {
-        Font titleFont = new Font("Arial", Font.BOLD, 23);
-        g.setColor(Color.red);
-        g.setFont(titleFont);
-        g.drawString("Processes Information", 700, 30);
-
-        int y = 50;
-        Font infoFont = new Font("Arial", Font.BOLD, 15);
-        g.setFont(infoFont);
-
-        for (Process process : processes) {
-            g.setColor(Color.BLACK);
-            g.drawString("Process: " + process.getName(), 700, y + 15);
-            g.setColor(process.getColor());
-            g.fillRect(800, y, 20, 20); // Display color in a square
-            g.setColor(Color.BLACK);
-            g.drawString("ID: " + process.getId(), 830, y + 15);
-            g.drawString("Priority: " + process.getPriority(), 890, y + 15);
-
-            y += 50;
-        }
-        // Displaying context switching time information
+    protected void displayProcessInfo(Graphics g){
+        super.displayProcessInfo(g);
         g.drawString("Context Switching Time: " + contextSwitchingTime, 700, y + 15);
         g.setColor(Color.BLACK);
         g.fillRect(900, y, 20, 20);
-    }
-
-    /**
-     * Displays statistics related to the scheduling algorithm.
-     *
-     * @param g Graphics object used for drawing
-     */
-    private void displayStatistics(Graphics g) {
-        Font titleFont = new Font("Arial", Font.BOLD, 18);
-        g.setColor(Color.red);
-        g.setFont(titleFont);
-        g.drawString("Statistics", 50, 400);
-        g.setColor(Color.BLACK);
-        g.drawString("Schedule Name: SJFSchedule", 50, 450);
-        g.setColor(Color.BLACK);
-        g.drawString("Average Waiting Time: " + averageWaitingTime, 50, 500);
-        g.setColor(Color.BLACK);
-        g.drawString("Average Turnaround Time: " + averageTurnaroundTime, 50, 550);
-    }
-
-    /**
-     * Calculates the maximum time among all processes.
-     *
-     * @return Maximum time among all processes
-     */
-    private int calculateMaxTime() {
-        int maxTime = 0;
-        for (Process process : processes) {
-            if (process.getBurstTime() > maxTime) {
-                maxTime = process.getBurstTime();
-            }
-        }
-        return maxTime;
     }
 }
