510c52c23137c2b35cd72e7adeba8613ab21d0b9
Shahd Mostafa <mostafashahd30@gmail.com>
2023-12-14 01:10:48 +0200

    made the draChart func loop over the time list

--- src/GUI.java
+++ src/GUI.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
  * Abstract class defining a GUI for displaying scheduling information.
  * Extends JFrame to create a window.
  */
-public abstract class GUI extends JFrame {
+public class GUI extends JFrame {
 
     protected ArrayList<Process> processes;
 
@@ -24,7 +24,7 @@ public abstract class GUI extends JFrame {
      * @param averageWaitingTime   Average waiting time for processes
      * @param averageTurnaroundTime Average turnaround time for processes
      */
-    public GUI(ArrayList<Process> processes, double averageWaitingTime, double averageTurnaroundTime) {
+    public GUI(ArrayList<Process> processes, double averageWaitingTime, double averageTurnaroundTime , String scheduleName) {
         this.processes = processes;
         this.averageWaitingTime = averageWaitingTime;
         this.averageTurnaroundTime = averageTurnaroundTime;
@@ -39,7 +39,7 @@ public abstract class GUI extends JFrame {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
-                drawChart(g); // Invoke method to draw the chart
+                drawChart(g , scheduleName); // Invoke method to draw the chart
             }
         };
 
@@ -55,7 +55,49 @@ public abstract class GUI extends JFrame {
      *
      * @param g Graphics object used for drawing
      */
-    protected abstract void drawChart(Graphics g);
+    /**
+     * Draws the Gantt chart representing the scheduling algorithm.
+     *
+     * @param g Graphics object used for drawing
+     */
+    protected void drawChart(Graphics g , String scheduleName) {
+        // Drawing the title
+        Font titleFont = new Font("Arial", Font.BOLD, 23);
+        g.setColor(Color.red);
+        g.setFont(titleFont);
+        g.drawString("CPU Scheduling Graph", 50, 30);
+
+        int y = 50;
+        int totalExecutionTime = calculateMaxTime();
+
+        // Find the maximum execution time among processes
+        for (Process process : processes) {
+            totalExecutionTime = Math.max(totalExecutionTime, process.getFinishedTime());
+        }
+
+        // Draw bars for each process
+        for (Process process : processes) {
+            //loop over the time list of the process
+            int executionStart = 0;
+            int executionEnd = 0;
+            for (int i = 0; i < process.getTime().size(); i++) {
+                executionStart = process.getTime().get(i).getKey();
+                executionEnd = process.getTime().get(i).getValue();
+                int barStart = (int) (((double) executionStart / totalExecutionTime) * 500);
+                int barLength = (int) (((double) (executionEnd - executionStart) / totalExecutionTime) * 500);
+
+                g.setColor(Color.BLACK);
+                g.drawString(process.getName(), 10, y + 20);
+
+                g.setColor(process.getColor());
+                g.fillRect(50 + barStart, y, barLength, 30);
+
+                y += 50;
+            }
+        }
+        displayProcessInfo(g); // Display process information
+        displayStatistics(g,scheduleName); // Display scheduling statistics
+    }
 
     /**
      * Method to display process information.
@@ -96,13 +138,13 @@ public abstract class GUI extends JFrame {
         Font titleFont = new Font("Arial", Font.BOLD, 18);
         g.setColor(Color.red);
         g.setFont(titleFont);
-        g.drawString("Statistics", 50, 400); // Title for statistics
+        g.drawString("Statistics", 50, 450); // Title for statistics
         g.setColor(Color.BLACK);
-        g.drawString("Schedule Name: " + scheduleName, 50, 450); // Display schedule name
+        g.drawString("Schedule Name: " + scheduleName, 50, 470); // Display schedule name
         g.setColor(Color.BLACK);
         g.drawString("Average Waiting Time: " + averageWaitingTime, 50, 500); // Display average waiting time
         g.setColor(Color.BLACK);
-        g.drawString("Average Turnaround Time: " + averageTurnaroundTime, 50, 550); // Display average turnaround time
+        g.drawString("Average Turnaround Time: " + averageTurnaroundTime, 50, 530); // Display average turnaround time
     }
 
     /**
